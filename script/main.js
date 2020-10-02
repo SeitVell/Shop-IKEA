598317b87a07977f5f95d5a33e9a4dbd73f60e6f
SeitVell <ssseitvell@gmail.com>
2020-10-02 21:35:53 +0300

    perenis is HTML

--- script/main.js
+++ script/main.js
@@ -1,12 +1,19 @@
-
+'use strict';
 //Day 1
 
 const btnBurger = document.querySelector ('.btn-burger');
 const catalog = document.querySelector ('.catalog');
-const overlay = document.querySelector ('.overlay');
 const btnClose = document.querySelector(' .btn-close');
 const subCatalog = document.querySelector ('.subcatalog');
-const subCatalogHeader =document.querySelector('.subcatalog-header')
+const subCatalogHeader =document.querySelector('.subcatalog-header');
+const btnReturn = document.querySelector('.btn-return');
+
+
+const overlay = document.createElement ('div');
+overlay.classList.add('overlay');
+document.body.insertAdjacentElement('beforeend', overlay);
+
+
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -14,6 +21,7 @@ const openMenu = () => {
 };
 
 const closeMenu = () => {
+    closeSubMenu();
     catalog.classList.remove('open');
     overlay.classList.remove('active');
 };
@@ -22,19 +30,20 @@ const closeMenu = () => {
     const itemList = event.target.closest('.catalog-list__item');
     if (itemList) {
         subCatalogHeader.innerHTML = itemList.textContent;
-        subCatalog.classList.add('subopen')
+        subCatalog.classList.add('subopen');
     }
  };
 
  const closeSubMenu = () => {
-
+    subCatalog.classList.remove('subopen');
  }
+
 
 btnBurger.addEventListener('click', openMenu);
 btnClose.addEventListener('click', closeMenu);
 overlay.addEventListener('click', closeMenu);
 catalog.addEventListener ('click', openSubMenu);
-
+btnReturn.addEventListener ('click', closeSubMenu);
 
 
 
